d3b398e5f07b2ed3646df716ede0565e63a8b5a8
Ivan Goncharov <Ivan.Goncharov@wexinc.co.nz>
2015-10-21 07:56:55 +1300

    Use OS-specific quotes in shell-command-to-string invocations

--- elisp/geiser-racket.el
+++ elisp/geiser-racket.el
@@ -363,7 +363,7 @@ using start-geiser, a procedure in the geiser/server module."
 (defvar geiser-racket-minimum-version "5.3")
 
 (defun geiser-racket--version (binary)
-  (shell-command-to-string (format "%s  -e '(display (version))'" binary)))
+  (shell-command-to-string (format "%s -e %s" binary (shell-quote-argument "(display (version))"))))
 
 (defvar geiser-racket--image-cache-dir nil)
 
